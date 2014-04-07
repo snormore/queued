a43ae1fb17463531f7de591e992a5ed663b56bfb
Steven Normore <snormore@gmail.com>
2014-04-06 21:47:20 -0400

    Clean up install.sh

--- install.sh
+++ install.sh
@@ -4,5 +4,6 @@ SRC_DIR=/usr/local/src/queued-$VERSION
 BIN_DIR=/usr/local/bin
 set -xe
 mkdir -p $SRC_DIR && cd $SRC_DIR
-wget https://raw.githubusercontent.com/snormore/queued/master/queued.sh
+curl -0 https://raw.githubusercontent.com/snormore/queued/master/queued.sh > queued.sh
+chmod +x queued.sh
 cp queued.sh $BIN_DIR/queued
