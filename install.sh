a3a478fcaddb610bbd7bedad0d6a14598601d9a8
Steven Normore <snormore@gmail.com>
2014-04-06 21:43:06 -0400

    Fix install.sh curl

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@ SRC_DIR=/usr/local/src/queued-$VERSION
 BIN_DIR=/usr/local/bin
 set -xe
 mkdir -p $SRC_DIR && cd $SRC_DIR
-curl -0 https://raw.githubusercontent.com/snormore/queued/master/queued.sh
+wget https://raw.githubusercontent.com/snormore/queued/master/queued.sh
 cp queued.sh $BIN_DIR/queued
